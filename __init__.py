c365f02db99ac98dac6807d034ca097db9f32033
Ludovic Tankeu <ludovic.tankeu64@gmail.com>
2019-11-24 23:06:48 +0100

    Initial commit, adding __init__.py content

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""STL package, implements common used C++ containers.
+"""
+
+__all__ = ['dequeue', 'forward_list', 'list', 'queue', 'stack', 'vector']
+
+__author__ = 'Ludovic Tankeu'
+
+__version__ = '1.0'
