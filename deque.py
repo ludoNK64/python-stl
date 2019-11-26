ebedc555c6456099503f513bcde0df5628ed02fe
Ludovic Tankeu <ludovic.tankeu64@gmail.com>
2019-11-26 22:24:03 +0100

    add deque's code with push_front and pop_front methods.

---
+++ deque.py
@@ -0,0 +1,26 @@
+"""Deque container.
+
+This is a subclass of vector but adds these methods:
+    push_front -> inserts element to the beginning
+    pop_front  -> removes the first element
+"""
+
+from .vector import vector
+
+class deque(vector):
+    """"""
+    def __init__(self, typecode, args=[], *, length=0, default=None):
+        """Constructor
+
+        Arguments are the same of vector.
+        """
+        _length, _default = length, default
+        super().__init__(typecode, args, length=_length, default=_default)
+
+    def push_front(self, value):
+        """Inserts element to the beginning."""
+        self.insert(0, value)
+
+    def pop_front(self):
+        """Removes the first element."""
+        self.erase(0)
