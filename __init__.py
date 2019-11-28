834f4016140554e7e8f5fe3e25ef74797cb11761
Ludovic Tankeu <ludovic.tankeu64@gmail.com>
2019-11-28 18:27:22 +0100

    remove support for 'forward_list' and 'list' in this version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """STL package, implements common used C++ containers.
 """
 
-__all__ = ['deque', 'forward_list', 'list', 'queue', 'stack', 'vector']
+__all__ = ['deque', 'queue', 'stack', 'vector']
 
 __author__ = 'Ludovic Tankeu'
 
